404352f21e7311d166e72834e836e796a85d7d90
alecaballero17 <alecaballeropereira@gmail.com>
2026-05-14 00:53:40 -0400

    refactor: actualizar el indicador de colección estática y eliminar la inserción de datos redundantes en el script de compilación.

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py loaddata render_seed.json
-
