a21e8ea87f0a3d6137a47097fa370d7103cbdb2b
brayanjoelrv <ramosvargabrayan@gmail.com>
2026-05-27 00:35:08 -0400

    fix: Hacer clinica nullable en Paciente para soportar pacientes huerfanos del app movil

--- apps/P2_Gestion_Clinica/models.py
+++ apps/P2_Gestion_Clinica/models.py
@@ -5,7 +5,7 @@ from apps.P1_Identidad_Acceso.models import Clinica
 # MÓDULO: GESTIÓN DE PACIENTES
 # ==============================================================================
 class Paciente(models.Model):
-    clinica = models.ForeignKey(Clinica, on_delete=models.CASCADE)
+    clinica = models.ForeignKey(Clinica, on_delete=models.CASCADE, null=True, blank=True)
 
     nombre = models.CharField(max_length=100)
     ci = models.CharField(max_length=20, unique=True)
