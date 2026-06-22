4f296cc771880c52908cda7230e9b11f87be9394
brayanjoelrv <ramosvargabrayan@gmail.com>
2026-06-22 02:28:52 -0400

    fix: fake migration 0003_usuario_debe_cambiar_password_and_more en Render para evitar error de columna duplicada

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Falso-aplicar migracion 0003 conflictiva porque la columna ya existe en la BD de Render
+python manage.py migrate P1_Identidad_Acceso 0003_usuario_debe_cambiar_password_and_more --fake || true
+
 # Ejecutar migraciones
 python manage.py migrate
 
